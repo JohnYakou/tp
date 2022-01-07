1137115a630ad04f4d21746bf0f1c8f9664cb30d
YakouJohn <jyakou@gmx.fr>
2022-01-07 16:11:56 +0100

    07/01/2022 16h11

---
+++ TP08 - John - JS/TP08-John/script.js
@@ -0,0 +1,44 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// btn addEventListener
+// set Attribute img =>
+
+// let prev = document.querySelector(".prev");
+// console.log(prev);
+// let next = document.querySelector(".next");
+// console.log(next);
+
+// prev.addEventListener("click",function(){
+//     // class_de_image.setattribut("src","le chemin de la nouvelle image");
+// })
+
+// faire une boucle for
